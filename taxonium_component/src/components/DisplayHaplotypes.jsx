c13d7492fdec3ba7c15753865f78bbde7d250980
pratikkatte <pratikkatte7@gmail.com>
2025-05-11 15:51:27 -0700

    uncertain nodes display toggle

--- taxonium_component/src/components/DisplayHaplotypes.jsx
+++ taxonium_component/src/components/DisplayHaplotypes.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 import { FaCircle, FaRegCircle } from "react-icons/fa";
 
-const DisplayHaplotypes = ({showUncertainNodes}) => {
+const DisplayHaplotypes = ({showUncertainNodes, search, singleSearchSpec}) => {
 
     const [displayNodes, setDisplayNodes] = useState(false)
+    const [searched, setSearched] = useState(false)
+    const searchKeys = useRef()
 
-    const toggleDisplay = () => {
+    const toggleDisplay = useCallback(() => {
+        
+        if(!searched){
+          var keys = showUncertainNodes()
+          
+          searchKeys.current = keys
+          setSearched(true)
+        }else{
+          if(searchKeys.current){
+            
+            search.setEnableUncertainNodes(searchKeys.current, !displayNodes)
+          }
+        }
         setDisplayNodes(!displayNodes)
-        showUncertainNodes()
-        console.log("clicked", displayNodes)
-    }
+        
+    }, [displayNodes, search.searchesEnabled, searchKeys])
 
     return (
         <button onClick={toggleDisplay} style={{padding: '0 0px 0px 10px'}} aria-label="Toggle Display Mode">
