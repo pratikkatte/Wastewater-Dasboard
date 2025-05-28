88459f7b7c3a27ef1f0271eab3304209d4d4a35a
pratikkatte <pratikkatte7@gmail.com>
2025-05-28 14:01:58 -0700

    record projects in workflow

---
+++ taxonium_backend/projects.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import json
+import os
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python projects.py <DIR> <TAXONIUM_FILE_PATH> <REF>")
+        sys.exit(1)
+    print(sys.argv)
+    DIR = sys.argv[1]
+    taxonium_file_path = sys.argv[2]
+    ref = sys.argv[3]
+
+    with open(ref+'.fai', 'r') as f:
+        ref_data = f.read().split('\t')
+    
+    print('data', ref_data)
+    reference_name = ref_data[0]
+    reference_start = 0
+    reference_end = ref_data[1]
+
+    path = '/workspace/results/projects.json'
+
+    if os.path.exists(path):
+        with open(path) as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    
+    data[DIR] = {
+        'taxonium_file_path': os.path.basename(taxonium_file_path),
+        'reference_name': reference_name,
+        'start': reference_start,
+        "end": reference_end,
+        'ref_file': os.path.basename(ref)
+
+    }
+
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+if __name__ == "__main__":
+    main()
